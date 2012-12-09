c913d5717ef4db0e3131f3e668cb0d07ab66cdb3
Richard Morrison <richard@rmorrison.net>
2012-12-09 16:56:21 +0000

    More flexibility for external node.js apps

--- index.js
+++ index.js
@@ -2,15 +2,31 @@ var prefixes = [];
 var hoard = require('hoard');
 var mkdirp = require('mkdirp');
 var path = require('path');
+var fldr_path;
 
 unixTime = function() {
   return parseInt(new Date().getTime() / 1000);
 };
 
 exports.hoard = hoard;
-exports.init = function(url_path, prefixes, express, app, fldr_path) {
-    prefixes = prefixes;
+exports.save_metric_value = function(metric_name, metric_value, timestamp, callback) {
+    var t = timestamp ? parseInt(timestamp) : unixTime();
+
+    if (prefixes.indexOf(metric_name) >= 0) {
+        hoard.update(path.join(fldr_path, metric_name + ".hoard"), metric_value, t, function(err) {
+            if (err) throw err;
+            if (callback) callback();
+        });
+    } else {
+        callback("Invalid metric name");
+    }
+}
+
+exports.init = function(url_path, p, express, app, fp) {
+    prefixes = p;
+    fldr_path = fp;
     app.use(url_path, express.bodyParser());
+
     mkdirp(fldr_path, function (err) {
         if (err) console.error(err)
         else {
@@ -29,17 +45,15 @@ exports.init = function(url_path, prefixes, express, app, fldr_path) {
             });
             app.post(url_path + "/publish", function(req, res) {
                 var metric_name = req.body.metric;
-                var measure_time = req.body.measure_time ? parseInt(req.body.measure_time) : unixTime();
                 var value = req.body.value;
 
-                if (prefixes.indexOf(metric_name) >= 0) {
-                    hoard.update(path.join(fldr_path, metric_name + ".hoard"), value, measure_time, function(err) {
-                        if (err) throw err;
+                exports.save_metric_value(metric_name, value, req.body.measure_time, function(e) {
+                    if (e) {
+                        res.send(404, {"error": e});
+                    } else {
                         res.send({"success": true});
-                    });
-                } else {
-                    res.send(404, {"error": "invalid prefix"});
-                }
+                    }
+                });
             });
             
         }
